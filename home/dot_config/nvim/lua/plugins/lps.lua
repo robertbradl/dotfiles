1f55c2008af7561af95a1006dfb1fb5a99eb4054
Robert Leonard Bradl <robert.bradl@icloud.com>
2025-08-07 21:12:01 +0200

    Projects and LSP setup

---
+++ home/dot_config/nvim/lua/plugins/lps.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        volar = {
+          init_options = {
+            vue = {
+              hybridMode = true,
+            },
+          },
+        },
+        vtsls = {},
+      },
+    },
+  },
+}
